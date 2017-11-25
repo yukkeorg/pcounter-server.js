1d1747539d32298ed92c37e677936c20175436ad
Yusuke Ohshima <github@yukke.org>
2017-11-26 01:57:14 +0900

    update

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+    server: {
+        port: 18888,
+        client_dir: "./public"
+    },
+    port: {
+        game: 0x01,
+        bonus: 0x02,
+        chance_time: 0x04,
+    }
+}
+
--- resource/js/app.js
+++ resource/js/app.js
@@ -1,63 +1,72 @@
+// vim: ft=javascript ts=2 sts=2 sw=2
 import '../css/style.scss'
 
-(($) => {
-  // vim: ft=javascript ts=2 sts=2 sw=2
-  "use strict"
+"use strict"
 
-  class AnimationCounter {
-    constructor(elem, duration=500, resolution=10) {
-      this.elem = elem
-
-      this.duration = duration
-      this.resolution = resolution
-      this.mspf = this.duration / this.resolution
-      this.progress = 0
-      this.current_value = 0
-      this.goal_value = 0
-      this.delta = 0
-    }
-
-    set_goal(value) {
-      this.goal_value = value
-      this.progress = 0
-
-      if(this.goal_value - this.current_value == 0) {
-        return
-      }
-
-      this.delta = (this.goal_value - this.current_value) / this.resolution
-      setTimeout(this._action.bind(this), this.mspf)
-    }
-
-    _action() {
-      this.progress += this.mspf
-      if(this.progress >= this.duration) {
-        this.current_value = this.goal_value
-      } else {
-        this.current_value += this.delta
-        setTimeout(this._action.bind(this), this.mspf)
-      }
-      this.elem.innerHTML = Math.floor(this.current_value)
-    }
-  }
-
-  const p_total = $('#pcount_total')
-  const p_current = $('#pcount_current')
-  const p_bonus = $('#pcount_bonus')
-  const p_bonus_chain = $('#pcount_bonus_chain')
-  const p_points = new AnimationCounter($('#pcount_points'), 1000)
-
-  const ws = new WebSocket('ws://localhost:18888')
-  ws.addEventListener('message', (message) => {
-    let c_status = JSON.parse(message.data)
-    p_total.innerHTML = c_status.total
-    p_current.innerHTML = c_status.current
-    p_bonus.innerHTML = c_status.bonus
-    p_bonus_chain.innerHTML = c_status.bonus_chain
-    p_points.set_goal(c_status.bonus * 1000)
-  })
-
-})((query, element) => {
+function $(query, element) {
   if(!element) element = document
   return element.querySelector(query)
+}
+
+class AnimationCounter {
+  constructor(elem, duration=500, resolution=10) {
+    this.elem = elem
+
+    this.duration = duration
+    this.resolution = resolution
+    this.mspf = this.duration / this.resolution
+    this.progress = 0
+    this.current_value = 0
+    this.goal_value = 0
+    this.delta = 0
+  }
+
+  set_goal(value) {
+    this.goal_value = value
+    this.progress = 0
+
+    if(this.goal_value - this.current_value == 0) {
+      return
+    }
+
+    this.delta = (this.goal_value - this.current_value) / this.resolution
+    setTimeout(this._action.bind(this), this.mspf)
+  }
+
+  _action() {
+    this.progress += this.mspf
+    if(this.progress >= this.duration) {
+      this.current_value = this.goal_value
+    } else {
+      this.current_value += this.delta
+      setTimeout(this._action.bind(this), this.mspf)
+    }
+    this.elem.innerHTML = Math.floor(this.current_value)
+  }
+}
+
+const p_counter = $('#pcounter')
+const p_total = $('#pcount_total')
+const p_current = $('#pcount_current')
+const p_bonus = $('#pcount_bonus')
+const p_bonus_chain = $('#pcount_bonus_chain')
+const p_points = new AnimationCounter($('#pcount_points'), 500, 20)
+
+const ws = new WebSocket('ws://localhost:18888')
+ws.addEventListener('message', (message) => {
+  let c_status = JSON.parse(message.data)
+
+  p_total.innerHTML = c_status.total
+  p_current.innerHTML = c_status.current
+  p_bonus.innerHTML = c_status.bonus
+  p_bonus_chain.innerHTML = c_status.bonus_chain
+  p_points.set_goal(c_status.bonus * 1000)
+
+  if(c_status.is_chancetime) {
+    p_counter.classList.remove('normal-game')
+    p_counter.classList.add('chance-time')
+  } else {
+    p_counter.classList.remove('chance-time')
+    p_counter.classList.add('normal-game')
+  }
 })
