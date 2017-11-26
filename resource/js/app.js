4143d56f94371e0f0d4c7ed8576688880a7112ac
Yusuke Ohshima <github@yukke.org>
2017-11-27 00:50:37 +0900

    update

--- resource/js/app.js
+++ resource/js/app.js
@@ -6,7 +6,7 @@ import '../css/style.scss'
 import AnimationCounter from './animationcounter.js'
 import $ from './selector.js'
 
-const p_counter = $('#pcounter')
+const p_title = $('#pcount_title')
 const p_total = $('#pcount_total')
 const p_current = $('#pcount_current')
 const p_bonus = $('#pcount_bonus')
@@ -15,22 +15,36 @@ const p_points = new AnimationCounter($('#pcount_points'), 500, 20)
 
 const ws = new WebSocket('ws://localhost:18888')
 ws.addEventListener('message', (message) => {
-  let c_status = JSON.parse(message.data)
+  const json = JSON.parse(message.data)
 
-  p_total.innerHTML = c_status.total
-  p_current.innerHTML = c_status.current
-  p_bonus.innerHTML = c_status.bonus
-  p_bonus_chain.innerHTML = c_status.bonus_chain
-  //p_points.set_goal(c_status.bonus * 1000)
-
-  if(c_status.is_bonustime) {
-    p_current.classList.add('chance-time')
-  } else {
-    p_current.classList.remove('chance-time')
+  if(!('type' in json)) {
+    return
   }
-  if(c_status.is_chancetime) {
-    p_bonus.classList.add('chance-time')
-  } else {
-    p_bonus.classList.remove('chance-time')
+
+  switch(json.type) {
+    case "counter":
+      p_total.innerHTML = json.total
+      p_current.innerHTML = json.current
+      p_bonus.innerHTML = json.bonus
+      p_bonus_chain.innerHTML = json.bonus_chain
+      //p_points.set_goal(json.bonus * 1000)
+
+      if(json.is_bonustime) {
+        p_current.classList.add('chance-time')
+      } else {
+        p_current.classList.remove('chance-time')
+      }
+      if(json.is_chancetime) {
+        p_bonus.classList.add('chance-time')
+      } else {
+        p_bonus.classList.remove('chance-time')
+      }
+      break
+
+    case "title":
+      if('title' in json) {
+        p_title.innerHTML = json.title
+      }
+      break
   }
 })
