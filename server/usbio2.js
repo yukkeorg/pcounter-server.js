2b4dfe3c12990004815bbe2a70ac13fb73a3bb8e
Yusuke Ohshima <ohshima.yusuke@yukke.org>
2020-10-24 11:59:02 +0900

    Add/Fix comments

--- server/usbio2.js
+++ server/usbio2.js
@@ -8,6 +8,7 @@ export class UsbIO2 extends EventEmmiter {
         this.inspect_interval = interval  // unit: ms
         this.RW_CMD = [0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
 
+        // USB-IO2.0 = VendorID: 0x1352, ProductID:0x0120
         this.device = new HID.HID(0x1352, 0x120)
         this.device.on('data', this._on_data.bind(this))
         this._prev_data = null
@@ -20,17 +21,22 @@ export class UsbIO2 extends EventEmmiter {
     }
 
     _on_data(data) {
-        // USB-IO2側の回路はプルアップされており、ON=0, OFF=1と
-        // なるため、取得したデータが反転していることを考慮する。
+        // USB-IO2側の回路がプルアップしており、
+        // ON、OFFが読み取ったデータ上0, 1となっているため、
+        // 反転して、補正する。
         let now_data = (~((data[2] << 8) + data[1])) & 0x7f
+
         if(now_data !== this._prev_data) {
             // ビットが 0 -> 1 になったところを検出
             // (prev_data XOR now_data) AND now_data
             const onbits = (this._prev_data ^ now_data) & now_data
+          //
             // ビットが 1 -> 0 になったところを検出
             // (prev_data XOR now_data) AND (NOT now_data)
             const offbits = (this._prev_data ^ now_data) & ~now_data
+
             this.emit('changed', now_data, onbits, offbits)
+
             this._prev_data = now_data
         }
     }
